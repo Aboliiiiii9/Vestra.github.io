5f1fc8ae772276c8a91bf27745f3f2083682b497
Aboliiiiii9 <abol81yari@gmail.com>
2026-06-27 18:04:21 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -93,4 +93,30 @@ card.style.display="none";
 
 });
 
+}const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
 }
